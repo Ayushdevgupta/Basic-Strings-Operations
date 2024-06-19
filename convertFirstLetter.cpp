2601a31dfeb1bdd5dfacb13fa3a491cd12291cae
Ayushdevgupta <ayushgupta620644@gmail.com>
2024-06-19 23:20:50 +0530

    Converting first letter of each word

---
+++ convertFirstLetter.cpp
@@ -0,0 +1,14 @@
+//WAP to input a string and convert the first letter of each word to uppercase.
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str[1000];
+    cout<<"Enter string"<<endl;
+    cin.getline(str,1000);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        
+    }   
+      
+}
